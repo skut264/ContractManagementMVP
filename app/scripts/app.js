9a2cc90e637bd7a1fd49c1cb5e3bf66c5d8be4b5
skut264 <129252351+skut264@users.noreply.github.com>
2023-04-05 18:40:08 +0530

    Update app.js
    test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,18 +9,18 @@ async function init() {
 
 async function renderAllData() {
   await renderText();
+  await renderButton();
 }
 
 async function renderText() {
   const nameElement = document.getElementById('nameText');
   const choursElement = document.getElementById('choursText');
   const finalElement = document.getElementById('finalText');
-  const cidElement = document.getElementById('cidText'); // added this line
+  const cidElement = document.getElementById('cidText');
   const contactData = await client.data.get('contact');
   const tData = await client.data.get('ticket');
   const tcreated = tData.ticket.created_at;
-  const ch = contactData.contact.custom_fields.ch;
-  //const cid = contactData.contact.company_id;
+  const buttonElement = document.getElementById('replyButton');
 
   const {
     contact: { name }
@@ -29,28 +29,61 @@ async function renderText() {
   console.log(tData);
 
   nameElement.innerHTML = `Ticket is created by ${name}`;
-  choursElement.innerHTML = `Ticket created on  ${tcreated}`;
-  //cexpElement.innerHTML = `Your Company Contract Ends on ${ch}`;
-  
+  choursElement.innerHTML = `Ticket created on ${tcreated}`;
+
   try {
-    const renewalDate = await getCompanyRenewalDate(); // Retrieve renewal date using the new function
+    const renewalDate = await getCompanyRenewalDate();
     cidElement.innerHTML = `Your Company Dash Ends on ${renewalDate}`;
-	if(tcreated<renewalDate)
-		finalElement.innerHTML = ` Contract Valid, Continue Working`
-	else
-		finalElement.innerHTML = ` Ticket la kai vecha Setha`
-		
+    if (tcreated < renewalDate){
+      finalElement.innerHTML = `Contract Valid, Continue Working`;
+	  buttonElement.style.display = 'none';
+	}
+    else
+	{
+      finalElement.innerHTML = `Ticket la kai vecha Setha`;
+	  buttonElement.style.display = 'block';
+      buttonElement.addEventListener('click', sendTicketReply);
+	}
   } catch (error) {
     console.error(error);
     cidElement.innerHTML = 'Could not retrieve company details.';
   }
 }
 
-async function getCompanyRenewalDate() {
-	const contactData = await client.data.get('contact');
-  const cid = contactData.contact.company_id;
-  const companyID = cid; // Replace with actual company ID
+
+async function sendTicketReply() {
+  const tData = await client.data.get('ticket');
+  const ticketId = tData.ticket.id;
   const apiKey = 'iy4J35AI0aJRCSxzAmc8'; // Replace with actual API key
+  const baseUrl = `https://benjaminsquare.freshdesk.com/api/v2/tickets/${ticketId}/reply`;
+  const headers = {
+    'Authorization': `Basic ${btoa(apiKey + ':x')}`,
+    'Content-Type': 'application/json'
+  };
+  const payload = {
+    body: 'Content of the reply goes here'
+  };
+  try {
+    const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+      console.log('Ticket reply sent successfully');
+    } else {
+      throw new Error(`Failed to send ticket reply. Status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getCompanyRenewalDate() {
+  const contactData = await client.data.get('contact');
+  const cid = contactData.contact.company_id;
+  const companyID = cid;
+  const apiKey = 'iy4J35AI0aJRCSxzAmc8';
   const baseUrl = `https://benjaminsquare.freshdesk.com/api/v2/companies/${companyID}`;
   const headers = {
     'Authorization': `Basic ${btoa(apiKey + ':x')}`
@@ -62,7 +95,7 @@ async function getCompanyRenewalDate() {
       const renewalDate = companyData.custom_fields.ch;
       return renewalDate;
     } else {
-      throw new Error(`Could not retrieve company details. Status : ${response.status}`);
+      throw new Error(`Could not retrieve company details. Status: ${response.status}`);
     }
   } catch (error) {
     console.error(error);
